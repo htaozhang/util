8fe1abc974f42ce33bf91f6b54e6680e2d66454e
macrozhang <macrozhang@tencent.com>
2018-08-02 17:33:13 +0800

    ...

---
+++ status_test.cc
@@ -0,0 +1,16 @@
+//
+// Copyright (C) 2018 macrozhang. All rights reserved.
+// Date：2018-08-02
+//
+
+#include "status.h"
+
+namespace util {
+
+}
+
+int main(int argc, char** argv) {
+
+  return 0;
+}
+
